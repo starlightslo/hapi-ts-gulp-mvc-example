c2b523d0bbcbb2754d3577c7e4ead4cabc11835c
Tony Huang <tony@huang.ninja>
2016-05-12 14:51:02 +0800

    Changed to use gulp-clean-css.

--- tasks/client/build_css.js
+++ tasks/client/build_css.js
@@ -1,13 +1,13 @@
 /*jshint esversion: 6 */
 
 const gulp = require('gulp');
-const minifyCSS = require('gulp-minify-css');
+const cleanCSS = require('gulp-clean-css');
 
 const path = require('../const').path;
 const tasks = require('../const').tasks;
 
 gulp.task(tasks.CLIENT_CSS_DIST, () => {
 	return gulp.src(path.CSS)
-		.pipe(minifyCSS())
+		.pipe(cleanCSS())
 		.pipe(gulp.dest(path.CLIENT_PUBLIC_DIST));
 });
